7964172532a33331b550c622d41cd577ec5e7c0d
22-JWL <jck1004ee@gmail.com>
2026-01-22 14:33:32 +0900

    fix : namecard css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,7 +33,7 @@ export default function Footer() {
             <li><span className="role">SUPPORT</span> 구합니다</li>
           </ul>
         </div> */}
-        <div className="footer-column">
+        <div>
           <object width="100%" height="auto" type="image/svg+xml" data="https://criminal-vivyanne-lucidus-346ca075.koyeb.app/lucidus/card_v2?name=Jaewook%20Lee&job=AI%20Engineer&company=AML.labs&address=Seoul%2C%20Korea&about=Powered%20by%20caffeine%20and%20anxiety.&linkedin=https%3A%2F%2Flinkedin.com%2Fin%2Fjaewook-lee-825a09383&github=https%3A%2F%2Fgithub.com%2F22-JWL&linkedin_color=%230077B5"></object>
         </div>
       </div>
